6e6bb36d0090a640cfdb31c673ad7904f43effea
tenpertur <tenpertur@gmail.com>
2025-07-28 00:48:45 +0200

    webpack config improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import {Box} from "components/box/Box"
 
 const App = () => {
 
   return <>
-     <div>Hello, world!</div>;
-     <Box size='medium' radius='small'></Box>
+     <div>Hello, world!</div>
   </>
-
-};
+}
 export default App;
 
--- webpack.common.js
+++ webpack.common.js
@@ -2,9 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin')
 
 module.exports = {
 
@@ -22,16 +24,11 @@ module.exports = {
                 },
             },
             {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-            {
-                test: /\.(s(a|c)ss)$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|eot|ttf|otf)$/,
-                type: 'asset/resource',
+                test: /\.(png|jpe?g|gif|svg|ico|woff|woff2|eot|ttf|otf)(\?v=\d\.\d\.\d)?$/,
+                type: "assets/resource",
+                generator: {
+                    filename: "assets/[name][ext]"
+                }
             },
             {
                 test: /\.(ts|tsx)$/,
@@ -48,47 +45,30 @@ module.exports = {
       },
 
     plugins: [
+        new ForkTsCheckerWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css',
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html',
+            filename: './index.html',
+            favicon: './public/assets/images/favicon.ico'
         }),
         new webpack.DefinePlugin({
         }),
+        new webpack.EnvironmentPlugin(['NODE_ENV']),
+        new CleanWebpackPlugin({verbose:true})
     ],
     optimization: {
-        minimize: true,
-        usedExports: true,
-        minimizer: [
-            new HtmlMinimizerPlugin(),
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ],
-        splitChunks: {
-            chunks: 'all',
-            minSize: 20000,
-            maxSize: 70000,
-            cacheGroups: {
-                defaultVendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    priority: -10,
-                    reuseExistingChunk: true,
-                },
-                default: {
-                    minChunks: 2,
-                    priority: -20,
-                    reuseExistingChunk: true,
-                },
-            },
-        },
-        runtimeChunk: 'single',
+        usedExports: true
+    },
+    watchOptions: {
+        ignored: '/node_modules/'
     },
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-
+        publicPath: '/'
     },
-
 };
